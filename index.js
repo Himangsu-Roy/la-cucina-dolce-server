e4ddda427b9d7cf4fcb64731ad93487fea5545ab
himangsu roy <himur98@gmail.com>
2023-05-02 16:32:32 +0600

    chef data res added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require("cors");
 const port = 5000;
 
+const chefs = require("./data/chefs.json");
+
+const recipes = require("./data/recipes.json");
 
 app.use(cors());
 
@@ -10,6 +13,10 @@ app.get("/", (req, res) => {
     res.send("Chef Recipe Hunter Server is Running");
 })
 
+
+app.get("/chefs", (req, res) => {
+    res.send(chefs)
+})
 
 
 
