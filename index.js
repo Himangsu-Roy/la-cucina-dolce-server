6eb7bd845a59a6fcfbb93668fb874c7d4370760d
himangsu roy <himur98@gmail.com>
2023-05-02 15:25:16 +0600

    inital response and port setting

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const express = require("express")
 const app = express();
+const cors = require("cors");
 const port = 5000;
+
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Chef Recipe Hunter Server is Running");
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Chef recipe hunter server is running on port: ${port}`)
+})
